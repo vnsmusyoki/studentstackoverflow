a16290d0d2780dd8a0d70d5c1344bbcf11e71832
Macbook _Pro <vnsmusyoki@gmail.com>
2022-11-26 05:59:05 +0300

    db connection

--- sign-up.php
+++ sign-up.php
@@ -31,18 +31,14 @@
  
    <?php include 'header.php'; ?>
  
-
  
-<!--======================================
-        START SIGN UP AREA
-======================================-->
 <section class="sign-up-area pt-80px pb-80px position-relative">
     <div class="container">
         <form action="#" class="card card-item">
             <div class="card-body row p-0">
                 <div class="col-lg-6">
                     <div class="form-content p-5 h-100 d-flex align-items-center justify-content-center flex-column bg-diagonal-gradient-primary-2 radius-top-left-8 radius-bottom-left-8 text-left">
-                        <h3 class="fs-35 pb-4 fw-bold text-white w-100">Join the Disilab Community</h3>
+                        <h3 class="fs-35 pb-4 fw-bold text-white w-100">Join the Student Stack Overflow Community</h3>
                         <div class="hero-list w-100">
                             <div class="d-flex align-items-center pb-30px">
                                 <svg width="26" height="26" class="mr-2"><path fill="#ffffff" opacity=".5" d="M4.2 4H22a2 2 0 012 2v11.8a3 3 0 002-2.8V5a3 3 0 00-3-3H7a3 3 0 00-2.8 2z"></path><path fill="#ffffff" d="M1 7c0-1.1.9-2 2-2h18a2 2 0 012 2v12a2 2 0 01-2 2h-2v5l-5-5H3a2 2 0 01-2-2V7zm10.6 11.3c.7 0 1.2-.5 1.2-1.2s-.5-1.2-1.2-1.2c-.6 0-1.2.4-1.2 1.2 0 .7.5 1.1 1.2 1.2zm2.2-5.4l1-.9c.3-.4.4-.9.4-1.4 0-1-.3-1.7-1-2.2-.6-.5-1.4-.7-2.4-.7-.8 0-1.4.2-2 .5-.7.5-1 1.4-1 2.8h1.9v-.1c0-.4 0-.7.2-1 .2-.4.5-.6 1-.6s.8.1 1 .4a1.3 1.3 0 010 1.8l-.4.3-1.4 1.3c-.3.4-.4 1-.4 1.6 0 0 0 .2.2.2h1.5c.2 0 .2-.1.2-.2l.1-.7.5-.7.6-.4z"></path></svg>
