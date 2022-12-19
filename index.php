8b508d2a00049f7bee1c579d50f4b145429283e5
Macbook _Pro <vnsmusyoki@gmail.com>
2022-12-19 17:51:34 +0300

    last changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Student Stack Overflow - Social Questions and Answers</title>
+    <title>Student Stack Overflow - HomePage</title>
 
     <!-- Google fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com/">
